dc4ae892c22b59590699c030823c6049d72a1251
Source-Kod <Source-Kod@protonmail.com>
2024-06-21 21:04:14 -0500

    vite setup

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake for weather app";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              nodejs
+            ];
+          };
+    };
+}
